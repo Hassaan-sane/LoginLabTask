24bec05a9d352bbb7f5e8a90dbddfef58a8e9298
Hassaan <hassaanbinsajjad03@gmail.com>
2017-10-01 14:25:17 +0500

    3rd LAB task

--- app/src/main/java/com/example/sane/madassignment/MainActivity.java
+++ app/src/main/java/com/example/sane/madassignment/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.sane.madassignment;
 
+import android.os.UserManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText Username, Password;
+        Button Login;
+
+        Username = (EditText) findViewById(R.id.username);
+        Password = (EditText) findViewById(R.id.password);
+        Login = (Button) findViewById(R.id.login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String struser = Username.getText().toString();
+                String strpass = Password.getText().toString();
+
+
+                if (struser.equals("")||strpass.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Please Enter Username or password", Toast.LENGTH_SHORT).show();
+                }
+                else if (struser.equals(strpass))
+                {
+                    Toast.makeText(MainActivity.this, "Username and Password are same", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Sum ting wong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
     }
 }
